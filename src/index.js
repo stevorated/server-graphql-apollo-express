5717387ef08318b522acbdd05b8f5a30136c9b46
stevorated <garbers8@gmail.com>
2019-07-15 17:37:59 +0300

    resave false

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ app.use(session({
   store,
   name: SESSION_NAME,
   secret: SESSION_SECRET,
-  resave: true,
+  // resave: true,
   httpOnly: IN_PROD,
   // rolling: true,
   saveUninitialized: false,

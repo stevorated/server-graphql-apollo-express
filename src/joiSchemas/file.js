77a77ff680fb501e04ae88717c78fcc30bd8be32
stevorated <garbers8@gmail.com>
2019-07-22 17:50:25 +0300

    changed upload limit to 4MB

--- src/joiSchemas/file.js
+++ src/joiSchemas/file.js
@@ -2,5 +2,5 @@ import Joi from './JoiImage'
 
 export const uploadImage = file => Joi.object().keys({
   mimetype: Joi.string().imageType().label('Invalid File Type'),
-  size: Joi.number().required().max(2000000).label('File too big')
+  size: Joi.number().required().max(4000000).label('File too big')
 })

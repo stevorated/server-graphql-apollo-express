9e37cedc10e7ff718bed48bf9ce3a38b881e85fb
stevorated <garbers8@gmail.com>
2019-07-24 16:25:55 +0300

    xxxxx

--- src/auth.js
+++ src/auth.js
@@ -70,8 +70,8 @@ export const facebookSignUpValidate = async (req, res) => {
   try {
     const user = await User.find({ fbId: req.id })
     // console.log(req.user)
-    req.session.userId = user.id
-    res.cookie('sid', user.id, { signed: true, httpOnly: true })
+    res.session.userId = user.id
+    // res.cookie('sid', user.id, { signed: true, httpOnly: true })
     // return res.redirect('https://wisdomofdecrowd.com')
   } catch (err) {
     return res.status(404).send('<h1>Something went wonnngg</h1>')

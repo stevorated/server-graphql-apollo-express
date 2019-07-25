26a0f90c07cc0a1a2d6598d3810789d0f198cf6c
stevorated <garbers8@gmail.com>
2019-07-25 21:41:47 +0300

    fixing fb login bugs

--- src/index.js
+++ src/index.js
@@ -114,7 +114,6 @@ passport.use(new FacebookStrategy({
   profileFields: ['id', 'name', 'email', 'picture']
 },
 async (accessToken, refreshToken, profile, cb) => {
-  console.log('======= refresh token ===========', refreshToken)
   console.log(profile._json)
   const { id, email, picture } = profile._json
   const givenName = profile.first_name

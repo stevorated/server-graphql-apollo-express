1ae544861ab4edb4a40aecb4199a613977b01b79
stevorated <garbers8@gmail.com>
2019-07-25 20:53:02 +0300

    fixing fb login bugs

--- src/index.js
+++ src/index.js
@@ -162,6 +162,7 @@ app.get(FB_LOGIN_CB_PATH,
   passport.authenticate('facebook', { failureRedirect: FB_LOGIN_FAIL_PATH }),
   async function (req, res, done) {
     // Successful authentication, redirect home.
+    console.log('success')
     return res.redirect(302, FB_SUCCESS_URL)
   })
 

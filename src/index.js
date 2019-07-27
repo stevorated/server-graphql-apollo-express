998b7b1e8dfadf6789545fcb52e2eec7532fa45e
stevorated <garbers8@gmail.com>
2019-07-27 16:56:35 +0300

    mobile upload

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ const server = new ApolloServer({
     }, // TODO: remember to block playground in prod
   uploads: {
     // maxFieldSize: 4000000,
-    maxFileSize: 4000000,
+    maxFileSize: 100000000,
     maxFiles: 10
   },
   context: ({ req, res }) => ({ req, res })

5b4f03dc5518dcd52dbd012b6206f0d01c87c7d4
stevorated <garbers8@gmail.com>
2019-07-27 17:44:07 +0300

    mobile upload

--- src/resolvers/file.js
+++ src/resolvers/file.js
@@ -20,12 +20,8 @@ export default {
   },
   Mutation: {
     singleUpload: async (root, args, { req }) => {
-      console.log(args)
       const session = req.session.passport ? req.session.passport.user : req.session
       const { userId } = session
-      console.log(userId)
-      // const { file, size, height, width, x, y } = args
-      // console.log({ aspect, height, width, unit, x, y })
       return processUpload(
         args,
         'avatar',
